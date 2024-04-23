699d4c04fb2b8b486888b7176fb5b5747eeb0d3e
gyf <charon0707@163.com>
2024-04-23 22:37:09 +0800

    update: it works now

--- data_process.py
+++ data_process.py
@@ -1,53 +1,57 @@
 import json
 import os
-from pprint import pprint
+import re
 
 
-def txt_to_json(lines):
-    json_data = []
-    current_entry = None
+def txt_to_json(txt_path):
+    MARK = ">>"  # some speech begin with two arrows
+    REGULAR = r"\d{14}\.\d{3}\|\d{14}\.\d{3}\|CC1\|"
+    STORY_SEG = "|SEG_00|Type=Story start"
+    SEG_COM = "|SEG_00|Type=Commercial"
 
-    for line in lines:
-        if line.strip():  # Make sure to skip empty lines
-            parts = line.strip().split('|')
-            if len(parts) > 3 and "SEG_" in parts[2]:  # Segment identifier in the third part
-                if current_entry:
-                    json_data.append(current_entry)  # Save the previous entry if exists
-                # Parse the segment line with type definition if present
-                segment_type = parts[3].split('=')[1] if '=' in parts[3] else "Unknown"
-                current_entry = {
-                    "start_time": parts[0],
-                    "end_time": parts[1],
-                    "segment": parts[2],
-                    "type": segment_type,
-                    "content": []
-                }
-            elif current_entry is not None and len(parts) > 3:
-                # Append content to the current segment
-                entry_content = {
-                    "time_range": f"{parts[0]}|{parts[1]}",
-                    "text": "|".join(parts[3:])  # Include all parts after the third as they are part of the text
-                }
-                current_entry["content"].append(entry_content)
+    # json format: {"time": "", "location": "", "language": "", "content": []}
+    json_data = {"time": "", "location": "", "language": "", "content": []}
 
-    if current_entry:
-        json_data.append(current_entry)  # Save the last entry
+    with open(txt_path, "r", encoding="utf-8") as file:
+        txt_content = file.read()
+    txt_content = re.sub(r">>>", MARK, txt_content)
 
+    segments = txt_content.split(STORY_SEG)
+    language = re.search(r"(LAN\|)(.*)", segments[0])[2]
+    time_location = re.search(r"(LBT\|)(.*)", segments[0])[2]
+    time = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2})(.*)", time_location)[1]
+    location = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2})(.*)", time_location)[2]
+    json_data["language"] = language
+    json_data["time"] = time
+    json_data["location"] = location
+
+
+    for seg in segments[1:]:
+        seg = seg.split(SEG_COM)[0].strip()
+        parts = seg.split(MARK)
+        content = []
+        for part in parts:
+            sentence = re.sub(REGULAR, "", part)
+            sentence = sentence.strip()
+            sentence = sentence.replace("\n", " ")
+            sentence = sentence.lower()
+            content.append(sentence)
+        json_data["content"].append("\n\n".join(content))
+
+
+    json_data = json.dumps(json_data)
+
+    json_name = txt_path.split("/")[-1].replace(".txt", ".json")
+    with open(os.path.join('jsons', json_name), 'w') as f:
+        f.write(json_data)
     return json_data
 
 
 if __name__ == "__main__":
-    file_path = '../2016_txt_data/2016-01-01_0000_US_CNN_Erin_Burnett_OutFront.txt'
 
-    with open(file_path, 'r', encoding='utf-8') as file:
-        txt_content = file.readlines()
+    file_path = "/txt/2016-01-01_0000_US_CNN_Erin_Burnett_OutFront.txt"
 
     # pprint(txt_content)
 
-    final_json = txt_to_json(txt_content)
+    txt_to_json(txt_path=file_path)
 
-    json_sample = json.dumps(final_json, indent=2) 
-    pprint(json_sample)
-    with open(os.path.join('jsons', 'output.json'), 'w') as f:
-        f.write(json_sample)
-    
--- gemma_2b_newsdata.py
+++ gemma_2b_newsdata.py
@@ -10,31 +10,58 @@ from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import CharacterTextSplitter
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from pprint import pprint
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Qdrant
 
 repo_id = "google/gemma-1.1-2b-it"
 
 llm = HuggingFaceEndpoint(
-    repo_id=repo_id, max_length=100, temperature=0.1, token=os.environ["HUGGINGFACEHUB_API_TOKEN"]
+    repo_id=repo_id, max_length=200, temperature=0.7, token=os.environ["HUGGINGFACEHUB_API_TOKEN"]
 )
 
 
 from langchain_community.document_loaders import JSONLoader
 loader = JSONLoader(
-    file_path="jsons/output.json",
-    jq_schema='.messages[].content',
-    # text_content=False
+    file_path="/content/drive/MyDrive/GSoC_RAG/output.json",
+    jq_schema='.content',
+    text_content=False
 )
 data = loader.load()
+
+
+# from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
+# from bs4 import BeautifulSoup as Soup
+# url = "https://transcripts.cnn.com/date/2024-04-13"
+
+# loader = RecursiveUrlLoader(
+#     url=url, max_depth=5, extractor=lambda x: Soup(x, "html.parser").text
+# )
+# data = loader.load()
+
+
+
 pprint(data)
 # split it into chunks
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 docs = text_splitter.split_documents(data)
 
 # create the open-source embedding function
-embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")# all-MiniLM-L6-v2
 
 # load it into Chroma
-db = Chroma.from_documents(docs, embedding_function)
+# db = Chroma.from_documents(docs, embedding_function)
+# create a qdrant collection - a vector based index of all resumes
+qdrant_collection = Qdrant.from_documents(
+    docs,
+    embedding_function,
+    location=":memory:", # Local mode with in-memory storage only
+    collection_name="resumes",
+)
+
+# construct a retriever on top of the vector store
+qdrant_retriever = qdrant_collection.as_retriever()
 
 memory = ConversationBufferMemory(memory_key = 'chat_history',return_messages=True)
 
@@ -49,11 +76,14 @@ CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
 conversational_chain = ConversationalRetrievalChain.from_llm(
             llm = llm,
             chain_type="stuff",
-            retriever=db.as_retriever(),
+            retriever=qdrant_retriever,
             memory = memory,
             condense_question_prompt=CUSTOM_QUESTION_PROMPT
         )
 
-# print(conversational_chain.invoke({"question":"who is gojo?"}))
-
-# print(conversational_chain.invoke({"question":"what is his power?"}))
+# pprint(conversational_chain.invoke({"question": "donald trump has proved what"}))
+# pprint(conversational_chain.invoke({"question": "what happened in 2015"}))
+# pprint(conversational_chain.invoke({"question": "how was 2015"}))
+# pprint(conversational_chain.invoke({"question": "2015 had what challenges"}))
+# pprint(conversational_chain.invoke({"question": "What are people talking about trump?"}))
+pprint(conversational_chain.invoke({"question": "you designed a car to cheat. it is really"}))
