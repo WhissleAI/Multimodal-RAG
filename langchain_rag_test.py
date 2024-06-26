374fea902fc8acb17034838747c3e095a513a75d
gyfffffff <0707charon@gmail.com>
2024-06-26 16:26:50 +0000

    add output example

--- langchain_rag_test.py
+++ langchain_rag_test.py
@@ -54,3 +54,46 @@ rag_chain = (
 )
 
 print(rag_chain.invoke("What is Task Decomposition?"))
+
+'''
+Human: You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know16:06:26 [8/1755$
+ say that you don't know. Use three sentences maximum and keep the answer concise.                                                                                
+Question: What is Task Decomposition?                                                                                                                             
+Context: Tree of Thoughts (Yao et al. 2023) extends CoT by exploring multiple reasoning possibilities at each step. It first decomposes the problem into multiple 
+thought steps and generates multiple thoughts per step, creating a tree structure. The search process can be BFS (breadth-first search) or DFS (depth-first search
+) with each state evaluated by a classifier (via a prompt) or majority vote.                                                                                      
+Task decomposition can be done (1) by LLM with simple prompting like "Steps for XYZ.\n1.", "What are the subgoals for achieving XYZ?", (2) by using task-specific 
+instructions; e.g. "Write a story outline." for writing a novel, or (3) with human inputs.                                                                        
+                                                                                                                                                                  
+(3) Task execution: Expert models execute on the specific tasks and log results.                                                                                  
+Instruction:                                                                                                                                                      
+                                                                                                                                                                  
+With the input and the inference results, the AI assistant needs to describe the process and results. The previous stages can be formed as - User Input: {{ User I
+nput }}, Task Planning: {{ Tasks }}, Model Selection: {{ Model Assignment }}, Task Execution: {{ Predictions }}. You must first answer the user's request in a str
+aightforward manner. Then describe the task process and show your analysis and model inference results to the user in the first person. If inference results conta
+in a file path, must tell the user the complete file path.                       
+
+Fig. 11. Illustration of how HuggingGPT works. (Image source: Shen et al. 2023)                                                                                   
+The system comprises of 4 stages:                                                
+(1) Task planning: LLM works as the brain and parses the user requests into multiple tasks. There are four attributes associated with each task: task type, ID, de
+pendencies, and arguments. They use few-shot examples to guide LLM to do task parsing and planning.
+Instruction:                            
+
+Finite context length: The restricted context capacity limits the inclusion of historical information, detailed instructions, API call context, and responses. The
+ design of the system has to work with this limited communication bandwidth, while mechanisms like self-reflection to learn from past mistakes would benefit a lot
+ from long or infinite context windows. Although vector stores and retrieval can provide access to a larger knowledge pool, their representation power is not as p
+owerful as full attention.              
+
+
+Challenges in long-term planning and task decomposition: Planning over a lengthy history and effectively exploring the solution space remain challenging. LLMs str
+uggle to adjust plans when faced with unexpected errors, making them less robust compared to humans who learn from trial and error. 
+Answer:          
+
+The previous stages can be formed as - User Input: {{ User Input }}, Task Planning: {{ Tasks }}, Model Selection: {{ Model Assignment }}, Task Execution: {{ Predi
+ctions }}. You must first answer the user's request in a straightforward manner. Then describe the task process and show your analysis and model inference results
+ to the user in the first person. If inference results contain a file path, must tell the user the complete file path.
+
+Fig. 11. Illustration of how HuggingGPT works. (Image source: Shen et al. 2023)                                                                                   
+The system comprises of 4 stages:                                                
+(1) Task planning: LLM works as the brain and parses the user requests into multiple tasks. There 
+'''
