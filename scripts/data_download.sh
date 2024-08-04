702af1b5d5e055cd419075a04b9e47072ee17af0
gyf <charon0707@163.com>
2024-08-04 16:53:20 +0800

    fix bugs

--- scripts/data_download.sh
+++ scripts/data_download.sh
@@ -1,6 +1,8 @@
+pip install gdown
+
 cd data/context_metadata
 
-gdown "https://drive.google.com/uc?1eARngAwANw9E28lX6tVsAoVz1Le7w3BA" -O 2016_01_english_with_metadata.csv
+gdown "https://drive.google.com/uc?id=1eARngAwANw9E28lX6tVsAoVz1Le7w3BA" -O 2016_01_english_with_metadata.csv
 
 gdown "https://drive.google.com/uc?id=1fdJ5oolRxFvyb1k6Zk9P9wpEPwGnYvV0" -O 2016_01_english_with_metadata.csv
 
