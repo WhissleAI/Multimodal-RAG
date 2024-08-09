a349836c2de0101c0506473aaf3ca3bd7487f0a6
gyf <charon0707@163.com>
2024-08-09 19:39:35 +0800

    fix bugs

--- src/eval.py
+++ src/eval.py
@@ -11,33 +11,34 @@ import csv
 
 
 async def aeval(eval_dataset, config, outpath):
-    metric_names = config['evaluation']['metrics']
-    metrics = []
-    metric_names = []
+    metric_names = config["evaluation"]["metrics"]
+    metrics = {}
     for metric_name in metric_names:
-        if (metric_name=="context_recall" or metric_name=="context_precision") and not config['use_rag']:
+        if (
+            metric_name == "context_recall" or metric_name == "context_precision"
+        ) and not config["use_rag"]:
             continue
-        module = importlib.import_module('ragas.metrics')
+        module = importlib.import_module("ragas.metrics")
         metric = getattr(module, metric_name)
-        metrics.append(metric)
-        metric_names.append(metric_name)
+        metrics[metric_name] = metric
 
     if not os.path.exists(f"{outpath}/eval.csv"):
-        with open(f"{outpath}/eval.csv", 'w') as f:
-            f.write(",".join(metric_names)) 
+        with open(f"{outpath}/eval.csv", "w") as f:
+            f.write(",".join(metrics.keys()))
 
     result = evaluate(
         eval_dataset,
-        metrics=metrics,
+        metrics=[metric for metric in metrics.values()],
         raise_exceptions=False,
     )
 
-    async with aiofiles.open(f"{outpath}/eval.csv", 'a+') as f:
+    async with aiofiles.open(f"{outpath}/eval.csv", "a+") as f:
         await f.write("\n")
         await f.write(",".join([str(item) for key, item in result.items()]))
 
+
 def get_avg_result(outpath):
-    data = np.genfromtxt(f"{outpath}/eval.csv", delimiter=',', skip_header=1)
+    data = np.genfromtxt(f"{outpath}/eval.csv", delimiter=",", skip_header=1)
     means = np.mean(data, axis=0)
     print("Column means:", means)
     with open(f"{outpath}/final_eval.csv") as f:
